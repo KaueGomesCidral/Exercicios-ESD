57129d49e01266da623221aff3da8b7c9fe73072
KAUE GOMES CIDRAL <kaue.cidral@univille.br>
2025-03-27 21:14:07 -0300

    Começo do Java

---
+++ listas-duplas-iteradores/vetores/VetorApp.java
@@ -0,0 +1,26 @@
+import estruturas.Vetor;
+
+
+public class VetorApp {
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+        
+        
+
+        vetor.adicionar(2);
+        vetor.adicionar(7);
+        vetor.adicionar(9);
+        vetor.adicionar(12);
+        vetor.adicionar(16);
+        vetor.adicionar(21);
+        vetor.adicionar(27);
+        vetor.adicionar(33);
+        vetor.adicionar(42);
+        vetor.adicionar(54);
+
+        vetor.adicionaraoinicio(5);
+    
+        vetor.mostrarElementos();
+    }
+}
